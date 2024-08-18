19813dd14116e09b927305b170fa4e7cfbb695fb
RHODELITCONSULT <rhodelitconsult@gmail.com>
2024-08-18 18:00:54 +0000

    navbar and footer update

--- resources/views/Frontend/includes/hero.blade.php
+++ resources/views/Frontend/includes/hero.blade.php
@@ -7,10 +7,10 @@
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-lg-7 pt-5">
-                                <h1 class="display-4 text-white mb-3 animated slideInDown">Let's Change The World With
-                                    Humanity</h1>
-                                <p class="fs-5 text-white-50 mb-5 animated slideInDown">Aliqu diam amet diam et eos.
-                                    Clita erat ipsum et lorem sed stet lorem sit clita duo justo erat amet</p>
+                                <h1 class="display-4 text-white mb-3 animated slideInDown">Welcome to Logos Outreach Ministries</h1>
+                                <p class="fs-5 text-white-50 mb-5 animated slideInDown">We extend a warm and heartfelt welcome to you. Our church is a sanctuary where love, compassion, 
+                                    and empowerment converge. We are a community dedicated to strengthening those who are weak, providing comfort to the masses, and uplifting 
+                                    the downtrodden.</p>
                                 <a class="btn btn-primary py-2 px-3 animated slideInDown" href="">
                                     Learn More
                                     <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
@@ -28,10 +28,31 @@
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-lg-7 pt-5">
-                                <h1 class="display-4 text-white mb-3 animated slideInDown">Let's Save More Lifes With
-                                    Our Helping Hand</h1>
-                                <p class="fs-5 text-white-50 mb-5 animated slideInDown">Aliqu diam amet diam et eos.
-                                    Clita erat ipsum et lorem sed stet lorem sit clita duo justo erat amet</p>
+                                <h1 class="display-4 text-white mb-3 animated slideInDown">Welcome to Logos Outreach Ministries</h1>
+                                <p class="fs-5 text-white-50 mb-5 animated slideInDown">We believe in the transformative power of Jesus Christ 
+                                    and strive to embody His love in our relationships with one another. Whether you are a long-time member or 
+                                    a first-time visitor, you are part of our family. We invite you to experience the warmth of our fellowship 
+                                    and the richness of our spiritual journey together.</p>
+                                <a class="btn btn-primary py-2 px-3 animated slideInDown" href="">
+                                    Learn More
+                                    <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
+                                        <i class="fa fa-arrow-right"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="w-100" src="{{asset('assets/Frontend/img/carousel-2.jpg')}}" alt="Image">
+                <div class="carousel-caption">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-7 pt-5">
+                                <h1 class="display-4 text-white mb-3 animated slideInDown">Welcome to Logos Outreach Ministries</h1>
+                                <p class="fs-5 text-white-50 mb-5 animated slideInDown">Let us journey together as we build a community where 
+                                    everyone feels valued, supported, and inspired to reach their full potential in Christ.</p>
                                 <a class="btn btn-primary py-2 px-3 animated slideInDown" href="">
                                     Learn More
                                     <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
